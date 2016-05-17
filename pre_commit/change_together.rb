363481f38da93bb1ae3739e86b3ca88356fa3977
Rhys <rimmington@gmail.com>
2016-05-17 10:27:27 +1000

    NEW: ChangeTogether

---
+++ pre_commit/change_together.rb
@@ -0,0 +1,27 @@
+require 'pathname'
+
+module Overcommit::Hook::PreCommit
+  # If any file in 'files' is changed, all must be.
+  class ChangeTogether < Base
+    def run
+      root = Pathname.new(execute(%w[git rev-parse --show-toplevel]).stdout.strip)
+      relative = applicable_files.map { |f| Pathname.new(f).relative_path_from(root).to_s }
+
+      modified = relative.select { |f| to_check.include? f }
+      if not modified.empty?
+        unmodified = to_check.select { |f| not relative.include? f }
+        if not unmodified.empty?
+          return [:fail, "Modified #{modified} but not #{unmodified}"]
+        end
+      end
+
+      return :pass
+    end
+
+    private
+
+    def to_check
+      @to_check ||= Array(config['files'])
+    end
+  end
+end
